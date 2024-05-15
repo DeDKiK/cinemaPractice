26fa725253d6a3661ac30d8be866425848843540
Vasyl Deresh <81803610+DeDKiK@users.noreply.github.com>
2024-05-15 18:51:08 +0300

    ContrellerFloder

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d6c5dddea1de58b46a461facf1ddd1f5a45970d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e587eded571d539f33b9163047e6a27eb72fb835")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
