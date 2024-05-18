1156586bebd93980c198682fadcdc704a8b86123
Orshovskyi Maksym <137319202+MaksymOrshovskyi@users.noreply.github.com>
2024-05-18 15:50:27 +0300

    Post

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0437ff95205defa345cb22db94618ff09fc58ef2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71579c522b221ab0afcb7d3cc46b2239399cee1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
